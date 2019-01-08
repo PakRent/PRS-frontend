2e78e70f896e9c9cd8993d78ca87765387fae4d0
Muhammad Nazam <muhammadnazam@Muhammads-MBP.attlocal.net>
2019-01-08 03:14:53 -0800

    setting components

--- src/components/Breadnav/Breadcrumb.js
+++ src/components/Breadnav/Breadcrumb.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import './Breadcrumb.css';
 
-const Breadcrumb = () =>{
+const Breadcrumb = (props) =>{
     return (
         <nav ariaLabel="breadcrumb">
             <ol className="breadcrumb">
-                <li className="breadcrumb-item"><i className="fas fa-home"></i><Link to="/">Home</Link></li>
-                <li ariaCurrent="page" className="breadcrumb-item active">User Login</li>
+                <li className="breadcrumb-item"><i className="fas fa-home"></i><Link to="/">{props.item}</Link></li>
+                <li ariaCurrent="page" className="breadcrumb-item active">{props.current}</li>
             </ol>
         </nav>
     )
--- src/pages/Login/Customer-login.js
+++ src/pages/Login/Customer-login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../components/Button/Button';
 import Breadcrumb from '../../components/Breadnav/Breadcrumb';
+import Input from '../../components/Forms/Formgroup';
 import './Customer-login.css';
 
 export default class CustomerLogin extends Component {
@@ -10,11 +11,14 @@ export default class CustomerLogin extends Component {
         <section className="hero-page bg-black-3">
             <div className="container">
               <h1 className="h2">Login / Create Account</h1>
-                <Breadcrumb /> 
+                <Breadcrumb
+                  item={"Home"}
+                  current={"User Login"}
+                /> 
             </div>
         </section>
         <section className="customer-login bg-black-2">
-          <div className="container">
+          <div className="container text-left">
             <div className="row">
               <div className="col-lg-6">
                 <h2 className="has-line">Login</h2>
@@ -22,15 +26,23 @@ export default class CustomerLogin extends Component {
                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
                 <hr />
                 <form action="#" className="login-form">
+                    <Input
+                    inputType={"email"}
+                    title={"Email *"}
+                    name={"email"}
+                    placeholder={"Type Your Email Address"}
+                    handleChange={this.handleInput}
+                  />{" "}
+                  <Input
+                    inputType={"password"}
+                    title={"Password *"}
+                    name={"password"}
+                    placeholder={"Password"}
+                    handleChange={this.handleInput}
+                  />{" "}
                   <div className="form-group">
-                    <input type="email" name="email" placeholder="Type your email address" className="form-control" />
-                  </div>
-                  <div className="form-group">
-                    <input type="password" name="paddword" placeholder="Password" className="form-control" />
-                  </div>
-                  <div className="form-group">
-                  <Button type={'submit'} className={"btn btn-gradient"} > Login </Button>
-                  </div>
+                  <Button type={'submit'} className={"btn btn-gradient"} title={"Login"}/>
+                 </div>
                 </form>
               </div>
               <div className="col-lg-6 mt-5 mt-lg-0">
@@ -49,7 +61,7 @@ export default class CustomerLogin extends Component {
                     <input type="password" name="password" placeholder="Password" className="form-control" />
                   </div>
                   <div className="form-group">
-                    <Button type={'submit'} className={"btn btn-gradient"} > Register </Button>
+                    <Button type={'submit'} className={"btn btn-gradient"} title={"Register"}/> 
                   </div>
                 </form>
               </div>
